f0bc3c91a3094fa85f16be34e52a00c2ceccd6b2
Tug Witt <tugw@pm.me>
2022-09-19 11:50:55 -0700

    add node prefix to worker metrics

--- svc/worker/metrics.go
+++ svc/worker/metrics.go
@@ -7,7 +7,7 @@ import (
 func initMetrics(env string, port int) (*metrics.Client, error) {
 	metricsClient := metrics.InitClient(port, true)
 	err := metricsClient.NewGauge("worker", "node_height", env,
-		"The last height for a given host", "url", "chain", "region")
+		"The last height for a given host", "node_url", "node_chain", "node_region")
 	if err != nil {
 		return nil, err
 	}
