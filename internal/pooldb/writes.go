cfbdc59ef1b863042631f5001a9f6df8b3782b6f
Tug Witt <tugw@pm.me>
2022-09-23 21:30:29 -0700

    finish up pooldb writes and integration tests

--- internal/pooldb/writes.go
+++ internal/pooldb/writes.go
@@ -9,8 +9,11 @@ import (
 
 func InsertNode(q dbcl.Querier, obj *Node) (uint64, error) {
 	const table = "nodes"
-	cols := []string{"url", "chain_id", "region", "version", "mainnet", "enabled", "backup",
-		"active", "synced", "height", "down_at", "backup_at"}
+	cols := []string{
+		"url", "chain_id", "region", "version", "mainnet",
+		"enabled", "backup", "active", "synced", "height",
+		"down_at", "backup_at",
+	}
 
 	return dbcl.ExecInsert(q, table, cols, obj)
 }
@@ -76,9 +79,12 @@ func UpdateIPAddressesSetInactive(q dbcl.Querier, duration time.Duration) error 
 
 func InsertRound(q dbcl.Querier, obj *Round) (uint64, error) {
 	const table = "rounds"
-	cols := []string{"chain_id", "miner_id", "height", "epoch_height", "uncle_height", "hash",
-		"nonce", "mix_digest", "coinbase_txid", "value", "difficulty", "luck", "accepted_shares", "rejected_shares",
-		"invalid_shares", "mature", "pending", "uncle", "orphan", "spent"}
+	cols := []string{
+		"chain_id", "miner_id", "height", "epoch_height", "uncle_height", "hash",
+		"nonce", "mix_digest", "coinbase_txid", "value", "difficulty", "luck",
+		"accepted_shares", "rejected_shares", "invalid_shares", "mature",
+		"pending", "uncle", "orphan", "spent",
+	}
 
 	return dbcl.ExecInsert(q, table, cols, obj)
 }
@@ -93,100 +99,6 @@ func UpdateRound(q dbcl.Querier, obj *Round, updateCols []string) error {
 func InsertShares(q dbcl.Querier, objects ...*Share) error {
 	const table = "shares"
 	cols := []string{"round_id", "miner_id", "count"}
-
-	rawObjects := make([]interface{}, len(objects))
-	for i, object := range objects {
-		rawObjects[i] = object
-	}
-
-	return dbcl.ExecBulkInsert(q, table, cols, rawObjects)
-}
-
-/* exchange batches */
-
-func InsertExchangeBatch(q dbcl.Querier, obj *ExchangeBatch) (uint64, error) {
-	const table = "exchange_batches"
-	cols := []string{"exchange_id", "status"}
-
-	return dbcl.ExecInsert(q, table, cols, obj)
-}
-
-func UpdateExchangeBatch(q dbcl.Querier, obj *ExchangeBatch, updateCols []string) error {
-	const table = "exchange_batches"
-	whereCols := []string{"id"}
-
-	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
-}
-
-func InsertExchangeInputs(q dbcl.Querier, objects ...*ExchangeInput) error {
-	const table = "exchange_batches"
-	cols := []string{"batch_id", "input_chain_id", "output_chain_id", "value"}
-
-	rawObjects := make([]interface{}, len(objects))
-	for i, object := range objects {
-		rawObjects[i] = object
-	}
-
-	return dbcl.ExecBulkInsert(q, table, cols, rawObjects)
-}
-
-func InsertExchangeDeposit(q dbcl.Querier, obj *ExchangeDeposit) (uint64, error) {
-	const table = "exchange_deposits"
-	cols := []string{"batch_id", "chain_id", "network_id", "deposit_txid", "exchange_txid",
-		"exchange_deposit_id", "value", "fees", "registered", "pending", "spent"}
-
-	return dbcl.ExecInsert(q, table, cols, obj)
-}
-
-func UpdateExchangeDeposit(q dbcl.Querier, obj *ExchangeDeposit, updateCols []string) error {
-	const table = "exchange_deposits"
-	whereCols := []string{"id"}
-
-	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
-}
-
-func InsertExchangeTrades(q dbcl.Querier, objects ...*ExchangeTrade) error {
-	const table = "exchange_trades"
-	cols := []string{"batch_id", "path_id", "stage", "exchange_trade_id", "next_trade_id",
-		"from_chain_id", "to_chain_id", "market", "direction", "increment", "value", "remainder",
-		"fees", "proceeds", "slippage", "initiated", "open", "filled"}
-
-	rawObjects := make([]interface{}, len(objects))
-	for i, object := range objects {
-		rawObjects[i] = object
-	}
-
-	return dbcl.ExecBulkInsert(q, table, cols, rawObjects)
-}
-
-func UpdateExchangeTrade(q dbcl.Querier, obj *ExchangeTrade, updateCols []string) error {
-	const table = "exchange_trades"
-	whereCols := []string{"id"}
-
-	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
-}
-
-func InsertExchangeWithdrawal(q dbcl.Querier, obj *ExchangeWithdrawal) (uint64, error) {
-	const table = "exchange_withdrawals"
-	cols := []string{"batch_id", "chain_id", "network_id", "exchange_txid", "exchange_withdrawal_id",
-		"value", "trade_fees", "withdrawal_fees", "pending", "spent"}
-
-	return dbcl.ExecInsert(q, table, cols, obj)
-}
-
-func UpdateExchangeWithdrawal(q dbcl.Querier, obj *ExchangeWithdrawal, updateCols []string) error {
-	const table = "exchange_withdrawals"
-	whereCols := []string{"id"}
-
-	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
-}
-
-/* balances */
-
-func InsertBalanceInputs(q dbcl.Querier, objects ...*BalanceInput) error {
-	const table = "balance_inputs"
-	cols := []string{"round_id", "chain_id", "miner_id", "out_chain_id",
-		"balance_output_id", "batch_id", "value", "pending"}
 
 	rawObjects := make([]interface{}, len(objects))
 	for i, object := range objects {
@@ -212,6 +124,158 @@ func InsertUTXOs(q dbcl.Querier, objects ...*UTXO) error {
 
 func UpdateUTXO(q dbcl.Querier, obj *UTXO, updateCols []string) error {
 	const table = "utxos"
+	whereCols := []string{"id"}
+
+	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
+}
+
+/* exchange batches */
+
+func InsertExchangeBatch(q dbcl.Querier, obj *ExchangeBatch) (uint64, error) {
+	const table = "exchange_batches"
+	cols := []string{"exchange_id", "status"}
+
+	return dbcl.ExecInsert(q, table, cols, obj)
+}
+
+func UpdateExchangeBatch(q dbcl.Querier, obj *ExchangeBatch, updateCols []string) error {
+	const table = "exchange_batches"
+	whereCols := []string{"id"}
+
+	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
+}
+
+func InsertExchangeInputs(q dbcl.Querier, objects ...*ExchangeInput) error {
+	const table = "exchange_batches"
+	cols := []string{"batch_id", "in_chain_id", "out_chain_id", "value"}
+
+	rawObjects := make([]interface{}, len(objects))
+	for i, object := range objects {
+		rawObjects[i] = object
+	}
+
+	return dbcl.ExecBulkInsert(q, table, cols, rawObjects)
+}
+
+func InsertExchangeDeposit(q dbcl.Querier, obj *ExchangeDeposit) (uint64, error) {
+	const table = "exchange_deposits"
+	cols := []string{
+		"batch_id", "chain_id", "network_id", "deposit_txid", "exchange_txid",
+		"exchange_deposit_id", "value", "fees", "registered", "confirmed", "spent",
+	}
+
+	return dbcl.ExecInsert(q, table, cols, obj)
+}
+
+func UpdateExchangeDeposit(q dbcl.Querier, obj *ExchangeDeposit, updateCols []string) error {
+	const table = "exchange_deposits"
+	whereCols := []string{"id"}
+
+	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
+}
+
+func InsertExchangeTrades(q dbcl.Querier, objects ...*ExchangeTrade) error {
+	const table = "exchange_trades"
+	cols := []string{
+		"batch_id", "path_id", "stage_id", "exchange_trade_id", "from_chain_id",
+		"to_chain_id", "market", "direction", "value", "proceeds", "trade_fees",
+		"cumulative_deposit_fees", "cumulative_trade_fees", "order_price", "fill_price",
+		"slippage", "initiated", "confirmed",
+	}
+
+	rawObjects := make([]interface{}, len(objects))
+	for i, object := range objects {
+		rawObjects[i] = object
+	}
+
+	return dbcl.ExecBulkInsert(q, table, cols, rawObjects)
+}
+
+func UpdateExchangeTrade(q dbcl.Querier, obj *ExchangeTrade, updateCols []string) error {
+	const table = "exchange_trades"
+	whereCols := []string{"id"}
+
+	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
+}
+
+func InsertExchangeWithdrawal(q dbcl.Querier, obj *ExchangeWithdrawal) (uint64, error) {
+	const table = "exchange_withdrawals"
+	cols := []string{
+		"batch_id", "chain_id", "network_id", "exchange_txid",
+		"exchange_withdrawal_id", "value", "deposit_fees", "trade_fees",
+		"withdrawal_fees", "cumulative_fees", "confirmed", "spent",
+	}
+
+	return dbcl.ExecInsert(q, table, cols, obj)
+}
+
+func UpdateExchangeWithdrawal(q dbcl.Querier, obj *ExchangeWithdrawal, updateCols []string) error {
+	const table = "exchange_withdrawals"
+	whereCols := []string{"id"}
+
+	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
+}
+
+/* balances */
+
+func InsertBalanceInputs(q dbcl.Querier, objects ...*BalanceInput) error {
+	const table = "balance_inputs"
+	cols := []string{
+		"round_id", "chain_id", "miner_id", "out_chain_id",
+		"balance_output_id", "batch_id", "value", "pending",
+	}
+
+	rawObjects := make([]interface{}, len(objects))
+	for i, object := range objects {
+		rawObjects[i] = object
+	}
+
+	return dbcl.ExecBulkInsert(q, table, cols, rawObjects)
+}
+
+func UpdateBalanceInput(q dbcl.Querier, obj *BalanceInput, updateCols []string) error {
+	const table = "balance_inputs"
+	whereCols := []string{"id"}
+
+	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
+}
+
+func InsertBalanceOutputs(q dbcl.Querier, objects ...*BalanceOutput) error {
+	const table = "balance_outputs"
+	cols := []string{
+		"chain_id", "miner_id", "in_deposit_id", "in_payout_id",
+		"out_payout_id", "value", "pool_fees", "exchange_fees",
+	}
+
+	rawObjects := make([]interface{}, len(objects))
+	for i, object := range objects {
+		rawObjects[i] = object
+	}
+
+	return dbcl.ExecBulkInsert(q, table, cols, rawObjects)
+}
+
+func UpdateBalanceOutput(q dbcl.Querier, obj *BalanceOutput, updateCols []string) error {
+	const table = "balance_outputs"
+	whereCols := []string{"id"}
+
+	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
+}
+
+/* payouts */
+
+func InsertPayout(q dbcl.Querier, obj *Payout) (uint64, error) {
+	const table = "payouts"
+	cols := []string{
+		"chain_id", "miner_id", "address", "txid", "height", "value",
+		"pool_fees", "exchange_fees", "tx_fees", "confirmed", "failed",
+	}
+
+	return dbcl.ExecInsert(q, table, cols, obj)
+}
+
+func UpdatePayout(q dbcl.Querier, obj *Payout, updateCols []string) error {
+	const table = "payouts"
 	whereCols := []string{"id"}
 
 	return dbcl.ExecUpdate(q, table, updateCols, whereCols, true, obj)
