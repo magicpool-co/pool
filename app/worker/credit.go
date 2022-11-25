4de594e303cd5989cac79e8ad5cd710fc1f418d5
Tug Witt <tugw@pm.me>
2022-11-25 12:37:11 -0800

    unlock but do not credit ergo blocks

--- app/worker/credit.go
+++ app/worker/credit.go
@@ -35,10 +35,6 @@ func (j *BlockUnlockJob) Run() {
 	defer lock.Release(ctx)
 
 	for _, node := range j.nodes {
-		if node.Chain() == "ERGO" {
-			continue
-		}
-
 		if err := credit.UnlockRounds(node, j.pooldb); err != nil {
 			j.logger.Error(fmt.Errorf("unlock: %s: %v", node.Chain(), err))
 			continue
