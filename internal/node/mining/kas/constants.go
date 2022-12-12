09a93f33bd69f5f82529050d1d0ce5c3089ad96a
Tug Witt <tugw@pm.me>
2022-12-11 23:51:39 -0800

    drop maturity depths for kaspa

--- internal/node/mining/kas/constants.go
+++ internal/node/mining/kas/constants.go
@@ -62,11 +62,11 @@ func (node Node) GetMaxDifficulty() *big.Int {
 }
 
 func (node Node) GetImmatureDepth() uint64 {
-	return 500
+	return 250
 }
 
 func (node Node) GetMatureDepth() uint64 {
-	return 5000
+	return 500
 }
 
 func (node Node) CalculateHashrate(blockTime, difficulty float64) float64 {
