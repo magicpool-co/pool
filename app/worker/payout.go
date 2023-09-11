bafe093cf2b429cf4b545b13c8cc5f50063546ce
Tug Witt <tugw@pm.me>
2023-09-10 19:34:56 -0700

    enable all payouts

--- app/worker/payout.go
+++ app/worker/payout.go
@@ -42,12 +42,6 @@ func (j *PayoutJob) Run() {
 	client := payout.New(j.pooldb, j.redis, j.telegram, j.mailer)
 
 	for _, node := range j.nodes {
-		switch node.Chain() {
-		case "ERG", "ERGO", "NEXA", "KAS", "ETC":
-		default:
-			continue
-		}
-
 		if err := client.InitiatePayouts(node); err != nil {
 			j.logger.Error(fmt.Errorf("payout: initiate: %s: %v", node.Chain(), err))
 		} else if err := client.FinalizePayouts(node); err != nil {
